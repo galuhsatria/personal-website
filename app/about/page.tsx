5cd2c70a1011f95d83511494d4b423262644150f
galuhsatria <galuhsatria219@gmail.com>
2023-09-27 18:04:02 +0800

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -25,7 +25,7 @@ export default function About() {
         </h1>
         <div className="flex flex-col gap-5 mt-6">
           <div data-fade="1">
-            <p>Hi, i&apos;m Galuh Satria You can call me Galuh, i&apos;m {age} years old. I started learning web development in December 2021, i learn web development from youtube, dicoding course, especially frontend development.</p>
+            <p>Hi, i&apos;m Galuh Satria You can call me Galuh, i&apos;m {age} years old. I started learning web development in December 2021, i learn web development from youtube, especially frontend development.</p>
             <p>Now Im studying at a university in Lombok, Nusa Tengara Barat majoring in computer sience from 2021, and I follow several communities on my campus such as the Google Developer Student Club</p>
             <p> I enjoy learning something new and getting feedback to make myself better and improve, and curently i learning NextJs.</p>
             <div>
