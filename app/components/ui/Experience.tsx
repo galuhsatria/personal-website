0d692519406d6c2916a460de604aae414e442507
galuhsatria <galuhsatria219@gmail.com>
2024-10-04 22:57:17 +0800

    add experience section

---
+++ app/components/ui/Experience.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FaBriefcase, FaGraduationCap, FaUsers } from 'react-icons/fa';
+
+
+export default function Experience() {
+  const events = [
+    {
+      date: 'July 2024 - August 2024',
+      title: 'Frontend Developer at DISKOMINFOTIK NTB',
+      description: 'Developing UI for web GIS to show area of blankspot in NTB and creating UI for chatbot',
+      icon: <FaBriefcase />,
+    },
+    {
+      date: 'Agustus 2023 - Desember 2023',
+      title: 'SIB Kampus Merdeka Frontend web and Backend at Dicoding',
+      description: 'Learn about web frontend and backend development through classes provided by dicoding and develop a project called Kasa Talk (website-based Sasak language dictionary)',
+      icon: <FaGraduationCap />,
+    },
+    {
+      date: 'September 2023 - September 2024',
+      title: 'PC Bumigora (organization)',
+      description: 'Participate as a committee in every event held. I also developed a website for the organization, I work as a fullstack in developing the website',
+      icon: <FaUsers />,
+    },
+  ];
+
+  return (
+    <div className="container mx-auto px-4 pt-8">
+      <div className="relative border-l border-gray-200">
+        {events.map((event, index) => (
+          <div key={index} className="mb-8 ml-4">
+            <div className="absolute w-8 h-8 rounded-full bg-black text-white dark:text-black dark:bg-white flex items-center justify-center -left-4">
+              {event.icon}
+            </div>
+            <div className="ml-8">
+              <time className="block text-xs text-muted-foreground">{event.date}</time>
+              <h3 className="font-semibold dark:text-white text-black">{event.title}</h3>
+              <p className="text-muted-foreground text-sm mt-1">{event.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,36 @@
 'use client';
 
-import Image from "next/image";
-import profilPicture from "@/assets/images/profile/profil-picture.jpeg";
-import { projects } from "./data/projects";
-import { FeaturedProject } from "./components/utils/ProjectContent";
-import Link from "next/link";
-import { HiOutlineArrowNarrowRight } from "react-icons/hi";
-import { BsGithub, BsTwitter } from "react-icons/bs";
-import useLoaded from "./hooks/useLoaded";
-
+import Image from 'next/image';
+import profilPicture from '@/assets/images/profile/profil-picture.jpeg';
+import { projects } from './data/projects';
+import { FeaturedProject } from './components/utils/ProjectContent';
+import Link from 'next/link';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
+import { BsGithub, BsTwitter } from 'react-icons/bs';
+import useLoaded from './hooks/useLoaded';
+import Experience from './components/ui/Experience';
 
 export default function Home() {
   const isLoaded = useLoaded();
   return (
     <main className="max-w-4xl mx-auto max-md:px-4 my-28">
-      <section className={`${isLoaded && "fade-in-start"}`}>
+      <section className={`${isLoaded && 'fade-in-start'}`}>
         <div className=" flex sm:flex-row flex-col sm:items-center gap-6 ">
           <Image src={profilPicture} alt="profil-picture" width={150} height={100} className="rounded-full object-contain" data-fade="0" placeholder="blur" />
           <div data-fade="1">
-            <h1 className="text-4xl font-bold">Galuh Satria</h1>
-            <p className="text-lg mt-2 font-medium text-muted-foreground transition-colors">Software Developer</p>
+            <h1 className="text-4xl font-bold ">Galuh Satria</h1>
+            <p className="text-lg mt-2 font-medium text-muted-foreground transition-colors">Frontend Developer</p>
           </div>
         </div>
         <div className="mt-8" data-fade="2">
           <h1 className="text-2xl font-bold mb-2">Hi Im Galuh</h1>
-          <p>Frontend web developer passionate about mastering modern technologies like Next.js and React Native for both web and mobile app development.  Seeking to level up my skills and contribute significantly to the field.</p>
+          <p>Frontend Developer who likes to build a product that can be useful for everyone. I usually work using technologies such as Next Js, Tailwind CSS, Prisma and some other great technologies.</p>
           <div className="flex gap-5 mt-4">
-            <Link href={"https://github.com/galuhsatria"} className="flex flex-row items-center gap-[6px] text-muted-foreground hover:text-primary transition-colors  group w-max">
+            <Link href={'https://github.com/galuhsatria'} className="flex flex-row items-center gap-[6px] text-muted-foreground hover:text-primary transition-colors  group w-max">
               <BsGithub className="group-hover:text-black dark:group-hover:text-white mt-1" />
               galuhsatria
             </Link>
-            <Link href={"https://twitter.com/galuhsatria___"} className="flex flex-row items-center gap-[6px] text-muted-foreground hover:text-primary transition-colors group w-max">
+            <Link href={'https://twitter.com/galuhsatria___'} className="flex flex-row items-center gap-[6px] text-muted-foreground hover:text-primary transition-colors group w-max">
               <BsTwitter className="group-hover:text-blue-500 mt-1" />
               galuhsatria___
             </Link>
@@ -43,6 +43,11 @@ export default function Home() {
           <Link href="/projects" className="flex items-center group mt-4 text-muted-foreground hover:text-primary transition-colors font-semibold w-max">
             See All Projects <HiOutlineArrowNarrowRight className="group-hover:ml-2 transition-all" />
           </Link>
+        </div>
+
+        <div data-fade="4" className='mt-12'>
+        <h1 className="text-2xl font-bold dark:text-white text-black">Experience</h1>
+          <Experience />
         </div>
       </section>
     </main>
