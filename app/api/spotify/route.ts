01180b591013746cb533346cacebedd1c949860e
galuhsatria <galuhsatria219@gmail.com>
2024-12-31 15:51:21 +0800

    fix chace

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -1,77 +1,85 @@
-import { NextRequest, NextResponse } from 'next/server';
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next/types';
 import querystring from 'querystring';
+import { NextResponse } from 'next/server';
 
 const { SPOTIFY_CLIENT_ID: client_id, SPOTIFY_CLIENT_SECRET: client_secret, SPOTIFY_REFRESH_TOKEN: refresh_token } = process.env;
 
-const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+const token = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
-interface SpotifyAccessTokenResponse {
-  access_token: string;
-}
-
-interface SpotifyCurrentlyPlayingResponse {
-  currently_playing_type: string;
+interface SpotifyData {
   is_playing: boolean;
   item: {
     name: string;
     album: {
       name: string;
-      artists: { name: string }[];
-      images: { url: string }[];
+      artists: Array<{ name: string }>;
+      images: [{ url: string }];
     };
-    external_urls: { spotify: string };
+    external_urls: {
+      spotify: string;
+    };
   };
+  currently_playing_type: string;
 }
 
-const getAccessToken = async (): Promise<string> => {
-  const response = await fetch(TOKEN_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${basic}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: querystring.stringify({
+const getAccessToken = async () => {
+  const res = await axios.post<{ access_token: string }>(
+    TOKEN_ENDPOINT,
+    querystring.stringify({
       grant_type: 'refresh_token',
       refresh_token,
     }),
-  });
+    {
+      headers: {
+        Authorization: `Basic ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }
+  );
 
-  const data: SpotifyAccessTokenResponse = await response.json();
-  return data.access_token;
+  return res.data.access_token;
 };
 
-const getNowPlaying = async (): Promise<Response> => {
+export const getNowPlaying = async () => {
   const access_token = await getAccessToken();
-  return fetch(NOW_PLAYING_ENDPOINT, {
+
+  return axios.get<SpotifyData>(NOW_PLAYING_ENDPOINT, {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
   });
 };
 
-export const GET = async (_: NextRequest): Promise<NextResponse> => {
+export async function GET() {
   const response = await getNowPlaying();
 
-  if (response.status === 204 || response.status > 400) {
-    return NextResponse.json({ isPlaying: false });
+  if (response.status === 204 || response.status > 400 || response.data.currently_playing_type !== 'track') {
+    // s-maxage=180 because a song usually lasts 3 minutes
+    return NextResponse.json(
+      { isPlaying: false },
+      {
+        headers: {
+          'Cache-Control': 'public, s-maxage=180, stale-while-revalidate=90',
+        },
+      }
+    );
   }
 
-  const data: SpotifyCurrentlyPlayingResponse = await response.json();
-
-  if (data.currently_playing_type !== 'track') {
-    return NextResponse.json({ isPlaying: false });
-  }
-
-  const nowPlayingData = {
-    isPlaying: data.is_playing,
-    title: data.item.name,
-    album: data.item.album.name,
-    artist: data.item.album.artists.map((artist) => artist.name).join(', '),
-    albumImageUrl: data.item.album.images[0].url,
-    songUrl: data.item.external_urls.spotify,
+  const data = {
+    isPlaying: response.data.is_playing,
+    title: response.data.item.name,
+    album: response.data.item.album.name,
+    artist: response.data.item.album.artists.map((artist) => artist.name).join(', '),
+    albumImageUrl: response.data.item.album.images[0].url,
+    songUrl: response.data.item.external_urls.spotify,
   };
 
-  return NextResponse.json(nowPlayingData);
-};
+  return NextResponse.json(data, {
+    headers: {
+      'Cache-Control': 'public, s-maxage=180, stale-while-revalidate=90',
+    },
+  });
+}
