9be76b6cfd90c33b9523ecbf4d929caeae115a1a
galuhsatria <galuhsatria219@gmail.com>
2025-04-05 17:15:28 +0800

    feat: zoom in and zoom out image

--- app/components/ui/ImageView.tsx
+++ app/components/ui/ImageView.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image';
 import React from 'react';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { ZoomIn , ZoomOut } from 'lucide-react';
 
 export default function ImageView({ src, alt, width, height, className }: { src: any; alt: string; width: number; height: number; className: string }) {
   return (
     <PhotoProvider
       toolbarRender={({ onScale, scale }) => {
         return (
-          <>
-            <svg className="PhotoView-Slider__toolbarIcon" onClick={() => onScale(scale + 1)} />
-            <svg className="PhotoView-Slider__toolbarIcon" onClick={() => onScale(scale - 1)} />
-          </>
+          <div className="flex gap-2">
+            <ZoomIn onClick={() => onScale(scale + 1)} className="cursor-pointer text-zinc-400 hover:text-white transition-colors duration-150" />
+            <ZoomOut onClick={() => onScale(scale - 1)} className="cursor-pointer text-zinc-400 hover:text-white transition-colors duration-150" />
+          </div>
         );
       }}
     >
