0563bf7e80d480a4058dcff79c8a42ee55a40edb
galuhsatria <galuhsatria219@gmail.com>
2025-04-25 22:09:54 +0800

    fix umami web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import NextTopLoader from "nextjs-toploader";
 const inter = Inter({ subsets: ["latin"] });
 import 'react-photo-view/dist/react-photo-view.css';
 import { GeistSans } from "geist/font/sans";
-import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: "Galuh Satria ",
@@ -22,7 +21,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-     <Script src="https://example.com/script.js" />
+      <head>
+        <script defer src="http://localhost:3000/script.js" data-website-id="de21237e-f7ab-4fc2-b75b-fb932ca4abf5"></script>
+      </head>
       <body className={GeistSans.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <NextTopLoader showSpinner={false} height={2} shadow="0 0 #0000" crawlSpeed={400}/>
