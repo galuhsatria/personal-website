c1fc6672c3b6c8ec99ad33c98ae30723e07ab4a8
galuhsatria <galuhsatria219@gmail.com>
2025-05-05 18:39:38 +0800

    change meta data function

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,11 +2,29 @@ import type { Metadata } from 'next';
 import { MDXContent } from '@content-collections/mdx/react';
 import { allPosts } from 'content-collections';
 import 'highlight.js/styles/github-dark-dimmed.css';
-import Head from 'next/head';
 
-export const metadata: Metadata = {
-  title: 'Galuh Satria | Blog',
-};
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+  const post = allPosts.find((p) => p.slug === params.slug);
+
+  if (!post) {
+    return {
+      title: 'Galuh Satria | Blog Tidak Ditemukan',
+    };
+  }
+
+  const ogImageUrl = process.env.NEXT_PUBLIC_WEB_DOMAIN + `/api/og?title=${encodeURIComponent(post.title)}&description=${encodeURIComponent(post.summary)}`;
+
+  return {
+    title: post.title,
+    openGraph: {
+      title: 'Galuh Satria | ' + post.title,
+      description: post.summary,
+      images: [ogImageUrl],
+      url: `${process.env.NEXT_PUBLIC_WEB_DOMAIN}/blog/${post.slug}`,
+      type: 'article',
+    },
+  };
+}
 
 const Page = async (props: { params: Promise<{ slug: string }> }) => {
   const params = await props.params;
@@ -24,29 +42,17 @@ const Page = async (props: { params: Promise<{ slug: string }> }) => {
     );
   }
 
-  const ogImageUrl = process.env.NEXT_PUBLIC_WEB_DOMAIN + `/api/og?title=${encodeURIComponent(post.title)}&description=${encodeURIComponent(post.summary)}`;
-
   return (
-    <>
-      <Head>
-        <title>{post.title}</title>
-        <meta property="og:title" content={post.title} />
-        <meta property="og:description" content={post.summary} />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_WEB_DOMAIN + `/blog/${post.slug}`} />
-        <meta property="og:type" content="article" />
-      </Head>
-      <main className="max-w-4xl mx-auto py-4 max-md:px-4 mt-7">
-        <div className="mt-14">
-          <h1 className="text-4xl max-md:text-2xl font-bold my-7">{post.title}</h1>
-          <p className="mt-2 text-muted-foreground">Written on {new Date(post.createdAt).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: '2-digit' })}</p>
-        </div>
+    <main className="max-w-4xl mx-auto py-4 max-md:px-4 mt-7">
+      <div className="mt-14">
+        <h1 className="text-4xl max-md:text-2xl font-bold my-7">{post.title}</h1>
+        <p className="mt-2 text-muted-foreground">Written on {new Date(post.createdAt).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: '2-digit' })}</p>
+      </div>
 
-        <article className="prose dark:text-white prose-th:text-blue-700 dark:prose-strong:text-white dark:prose-blockquote:text-white dark:prose-headings:text-white prose-headings:font-bold prose-a:text-blue-700 dark:prose-code:text-white pt-4">
-          <MDXContent code={post.mdx} />
-        </article>
-      </main>
-    </>
+      <article className="prose dark:text-white prose-th:text-blue-700 dark:prose-strong:text-white dark:prose-blockquote:text-white dark:prose-headings:text-white prose-headings:font-bold prose-a:text-blue-700 dark:prose-code:text-white pt-4">
+        <MDXContent code={post.mdx} />
+      </article>
+    </main>
   );
 };
 
