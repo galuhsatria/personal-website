07dcacceeb90262813298ef48ef3bd8429916706
galuhsatria <galuhsatria219@gmail.com>
2025-09-11 18:04:49 +0800

    fix: intl

--- app/projects/projects.tsx
+++ app/projects/projects.tsx
@@ -2,15 +2,17 @@
 import useLoaded from '@/hooks/useLoaded';
 import { AllProjects } from '../components/ProjectContent';
 import { projects } from '../data/projects';
+import { useTranslations } from 'next-intl';
 
 export default function Projects() {
   const isLoaded = useLoaded();
+  const t = useTranslations('projects');
 
   return (
     <main>
       <section className={`mt-28 mb-20 max-w-4xl max-lg:px-4 mx-auto ${isLoaded && 'fade-in-start'}`}>
         <h1 className="text-4xl font-bold " data-fade="1">
-          <span className="dark:text-white text-black">Projects</span>
+          <span className="dark:text-white text-black">{t('title')}</span>
         </h1>
         <AllProjects projects={projects} />
       </section>
