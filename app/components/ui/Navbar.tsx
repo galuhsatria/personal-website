fa8055a0291460eba45dda2712c8f40c224feb2a
galuhsatria <galuhsatria219@gmail.com>
2025-11-08 11:31:38 +0800

    feat: monanya project

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -1,14 +1,15 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { RxMoon, RxSun } from 'react-icons/rx';
-import { useTheme } from 'next-themes';
-import { Button } from './button';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { SiGithub, SiLinkedin } from 'react-icons/si';
-import { LanguageSelector } from '../LenguageSelector';
-import { useTranslations } from 'next-intl';
+import Link from "next/link";
+import { RxMoon, RxSun } from "react-icons/rx";
+import { useTheme } from "next-themes";
+import { Button } from "./button";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { SiGithub, SiLinkedin } from "react-icons/si";
+import { LanguageSelector } from "../LenguageSelector";
+import { useTranslations } from "next-intl";
+import { Separator } from "@/app/components/ui/separator";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
@@ -16,24 +17,24 @@ export default function Navbar() {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(false);
   const pathname = usePathname();
-  const t = useTranslations('navigation');
+  const t = useTranslations("navigation");
 
   const links = [
     {
-      label: t('home'),
-      href: '/',
+      label: t("home"),
+      href: "/",
     },
     {
-      label: t('projects'),
-      href: '/projects',
+      label: t("projects"),
+      href: "/projects",
     },
     {
-      label: t('blog'),
-      href: '/blog',
+      label: t("blog"),
+      href: "/blog",
     },
     {
-      label: t('about'),
-      href: '/about',
+      label: t("about"),
+      href: "/about",
     },
   ];
 
@@ -52,17 +53,19 @@ export default function Navbar() {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   });
 
   const changeTheme = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    setTheme(theme === "dark" ? "light" : "dark");
   };
 
   return (
-    <header className={`w-full mx-auto border-b border-border px-4 py-2 fixed top-0 z-50 transition-transform duration-30 bg-background ${visible ? 'max-sm:-translate-y-24' : ''}`}>
+    <header
+      className={`w-full mx-auto border-b border-border px-4 py-2 fixed top-0 z-50 transition-transform duration-30 bg-background ${visible ? "max-sm:-translate-y-24" : ""}`}
+    >
       <nav className="flex items-center max-w-4xl mx-auto rounded-full justify-between">
         <div>
           <ul className="hidden sm:flex gap-6">
@@ -70,7 +73,7 @@ export default function Navbar() {
               <li key={`${href}${label}`}>
                 <Link
                   href={href}
-                  className={`dark:hover:text-white text-base hover:text-black rounded-md hover:transition-colors ${pathname === href || (pathname.startsWith('/blog/') && href === '/blog') ? 'dark:text-white text-black' : 'text-zinc-400'}`}
+                  className={`dark:hover:text-white text-base hover:text-black rounded-md hover:transition-colors ${pathname === href || (pathname.startsWith("/blog/") && href === "/blog") ? "dark:text-white text-black" : "text-zinc-400"}`}
                 >
                   {label}
                 </Link>
@@ -79,15 +82,33 @@ export default function Navbar() {
           </ul>
         </div>
         <div className="flex items-center gap-2">
-          <Link href="https://www.linkedin.com/in/galuhsatria/" target="_blank" className="px-2 max-sm:hidden">
+          <Link
+            href="https://www.linkedin.com/in/galuhsatria/"
+            target="_blank"
+            className="px-2 max-sm:hidden"
+          >
             <SiLinkedin />
           </Link>
-          <Link href="https://github.com/galuhsatria" target="_blank" className="px-2 max-sm:hidden">
+          <Link
+            href="https://github.com/galuhsatria"
+            target="_blank"
+            className="px-2 max-sm:hidden"
+          >
             <SiGithub />
+            <Separator orientation="vertical" />
           </Link>
           <LanguageSelector iconOnly />
-          <Button variant="ghost" size="icon" className="bg-transparent" onClick={changeTheme}>
-            {mounted ? <>{theme === 'light' ? <RxMoon /> : <RxSun />}</> : <RxSun />}
+          <Button
+            variant="ghost"
+            size="icon"
+            className="bg-transparent"
+            onClick={changeTheme}
+          >
+            {mounted ? (
+              <>{theme === "light" ? <RxMoon /> : <RxSun />}</>
+            ) : (
+              <RxSun />
+            )}
           </Button>
         </div>
       </nav>
--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -1,89 +1,176 @@
-import waHelper from '@/assets/images/projects/wa-helper.png';
-import perkakas from '@/assets/images/projects/perkakas.png';
-import quranQuiz from '@/assets/images/projects/quran-quiz.png';
-import kasaTalk from '@/assets/images/projects/kasa-talk.png';
-import pcbumigora from '@/assets/images/projects/pcbumigora.png';
-import cataai from '@/assets/images/projects/cata-ai.png';
+import waHelper from "@/assets/images/projects/wa-helper.png";
+import perkakas from "@/assets/images/projects/perkakas.png";
+import quranQuiz from "@/assets/images/projects/quran-quiz.png";
+import kasaTalk from "@/assets/images/projects/kasa-talk.png";
+import pcbumigora from "@/assets/images/projects/pcbumigora.png";
+import cataai from "@/assets/images/projects/cata-ai.png";
+import monanya from "@/assets/images/projects/monanya.png";
 
-import { SiNextdotjs, SiReact, SiTailwindcss, SiFirebase, SiPlanetscale, SiExpress, SiSupabase, SiPrisma, SiFastapi, SiTensorflow } from 'react-icons/si';
+import {
+  SiNextdotjs,
+  SiReact,
+  SiTailwindcss,
+  SiFirebase,
+  SiPlanetscale,
+  SiExpress,
+  SiSupabase,
+  SiPrisma,
+  SiFastapi,
+  SiTensorflow,
+  SiReacthookform,
+  SiPostgresql,
+} from "react-icons/si";
 
-import { ProjectsProps } from '../types';
+import { ProjectsProps } from "../types";
+
+const DrizzleIcon = () => {
+  return (
+    <svg
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <title>Drizzle</title>
+      <path d="M5.353 11.823a1.036 1.036 0 0 0-.395-1.422 1.063 1.063 0 0 0-1.437.399L.138 16.702a1.035 1.035 0 0 0 .395 1.422 1.063 1.063 0 0 0 1.437-.398l3.383-5.903Zm11.216 0a1.036 1.036 0 0 0-.394-1.422 1.064 1.064 0 0 0-1.438.399l-3.382 5.902a1.036 1.036 0 0 0 .394 1.422c.506.283 1.15.104 1.438-.398l3.382-5.903Zm7.293-4.525a1.036 1.036 0 0 0-.395-1.422 1.062 1.062 0 0 0-1.437.399l-3.383 5.902a1.036 1.036 0 0 0 .395 1.422 1.063 1.063 0 0 0 1.437-.399l3.383-5.902Zm-11.219 0a1.035 1.035 0 0 0-.394-1.422 1.064 1.064 0 0 0-1.438.398l-3.382 5.903a1.036 1.036 0 0 0 .394 1.422c.506.282 1.15.104 1.438-.399l3.382-5.902Z" />
+    </svg>
+  );
+};
+
+const BetterAuthIcon = () => {
+  return (
+    <svg
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <title>Better Auth</title>
+      <path d="M0 3.39v17.22h5.783V15.06h6.434V8.939H5.783V3.39ZM12.217 8.94h5.638v6.122h-5.638v5.548H24V3.391H12.217Z" />
+    </svg>
+  );
+};
+
+const ShadcnUiIcon = () => {
+  return (
+    <svg
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <title>shadcn/ui</title>
+      <path d="M22.219 11.784 11.784 22.219c-.407.407-.407 1.068 0 1.476.407.407 1.068.407 1.476 0L23.695 13.26c.407-.408.407-1.069 0-1.476-.408-.407-1.069-.407-1.476 0ZM20.132.305.305 20.132c-.407.407-.407 1.068 0 1.476.408.407 1.069.407 1.476 0L21.608 1.781c.407-.407.407-1.068 0-1.476-.408-.407-1.069-.407-1.476 0Z" />
+    </svg>
+  );
+};
 
 export const projects: ProjectsProps[] = [
   {
     id: 1,
     src: waHelper,
-    title: 'Wa Helper',
-    description: 'items.waHelper.description',
+    title: "Wa Helper",
+    description: "items.waHelper.description",
     techs: [
-      { name: 'React', icon: <SiReact /> },
-      { name: 'Tailwind', icon: <SiTailwindcss /> },
+      { name: "React", icon: <SiReact /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
     ],
-    code: 'https://github.com/galuhsatria/whatsapp-messages-helper',
-    visit: 'https://whatsapp-messages-helper.vercel.app/',
+    code: "https://github.com/galuhsatria/whatsapp-messages-helper",
+    visit: "https://whatsapp-messages-helper.vercel.app/",
   },
   {
     id: 2,
     src: perkakas,
-    title: 'Tools',
-    description: 'items.perkakas.description',
+    title: "Tools",
+    description: "items.perkakas.description",
     techs: [
-      { name: 'Next Js', icon: <SiNextdotjs /> },
-      { name: 'Tailwind', icon: <SiTailwindcss /> },
+      { name: "Next Js", icon: <SiNextdotjs /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
     ],
-    code: 'https://github.com/galuhsatria/perkakas',
-    visit: 'https://perkakas-galuhsatria.vercel.app/',
+    code: "https://github.com/galuhsatria/perkakas",
+    visit: "https://perkakas-galuhsatria.vercel.app/",
   },
   {
     id: 3,
     src: quranQuiz,
-    title: 'Quran Quiz',
-    description: 'items.quranQuiz.description',
+    title: "Quran Quiz",
+    description: "items.quranQuiz.description",
     techs: [
-      { name: 'Next Js', icon: <SiNextdotjs /> },
-      { name: 'Tailwind', icon: <SiTailwindcss /> },
+      { name: "Next Js", icon: <SiNextdotjs /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
     ],
-    code: 'https://github.com/galuhsatria/quran-quiz',
-    visit: 'https://alquran-quiz.vercel.app/',
+    code: "https://github.com/galuhsatria/quran-quiz",
+    visit: "https://alquran-quiz.vercel.app/",
   },
   {
     id: 4,
-    src: kasaTalk,
-    title: 'Kasa Talk',
-    description: 'items.kasaTalk.description',
+    src: monanya,
+    title: "Monanya",
+    description: "items.monanya.description",
     techs: [
-      { name: 'Next Js', icon: <SiNextdotjs /> },
-      { name: 'Tailwind', icon: <SiTailwindcss /> },
-      { name: 'Firebase', icon: <SiFirebase /> },
-      { name: 'Planetscale', icon: <SiPlanetscale /> },
-      { name: 'Express JS', icon: <SiExpress /> },
+      { name: "Next Js", icon: <SiNextdotjs /> },
+      { name: "React Hook Form", icon: <SiReacthookform /> },
+      {
+        name: "Drizzle",
+        icon: <DrizzleIcon />,
+      },
+      {
+        name: "Better Auth",
+        icon: <BetterAuthIcon />,
+      },
+      {
+        name: "PostgreSQL",
+        icon: <SiPostgresql />,
+      },
+      {
+        name: "Shadcn/ui",
+        icon: <ShadcnUiIcon />,
+      },
     ],
-    visit: 'https://kasatalk.vercel.app/',
+    code: "https://github.com/galuhsatria/monanya",
+    visit: "https://monanya.vercel.app/",
   },
   {
     id: 5,
-    src: pcbumigora,
-    title: 'PC Bumigora',
-    description: 'items.pcbumigora.description',
+    src: kasaTalk,
+    title: "Kasa Talk",
+    description: "items.kasaTalk.description",
     techs: [
-      { name: 'Next Js', icon: <SiNextdotjs /> },
-      { name: 'Tailwind', icon: <SiTailwindcss /> },
-      { name: 'Prisma', icon: <SiPrisma /> },
-      { name: 'Supabase', icon: <SiSupabase /> },
+      { name: "Next Js", icon: <SiNextdotjs /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+      { name: "Firebase", icon: <SiFirebase /> },
+      { name: "Planetscale", icon: <SiPlanetscale /> },
+      { name: "Express JS", icon: <SiExpress /> },
     ],
-    visit: 'https://www.pcbumigora.online/',
+    visit: "https://kasatalk.vercel.app/",
   },
   {
     id: 6,
-    src: cataai,
-    title: 'Cata AI',
-    description: 'items.cataai.description',
+    src: pcbumigora,
+    title: "PC Bumigora",
+    description: "items.pcbumigora.description",
     techs: [
-      { name: 'React Js', icon: <SiReact /> },
-      { name: 'Tailwind', icon: <SiTailwindcss /> },
-      { name: 'Fast API', icon: <SiFastapi /> },
-      { name: 'Tensorflow', icon: <SiTensorflow /> },
+      { name: "Next Js", icon: <SiNextdotjs /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+      { name: "Prisma", icon: <SiPrisma /> },
+      { name: "Supabase", icon: <SiSupabase /> },
     ],
-    visit: 'https://cata-ai.vercel.app/',
+    visit: "https://www.pcbumigora.online/",
+  },
+  {
+    id: 7,
+    src: cataai,
+    title: "Cata AI",
+    description: "items.cataai.description",
+    techs: [
+      { name: "React Js", icon: <SiReact /> },
+      { name: "Tailwind", icon: <SiTailwindcss /> },
+      { name: "Fast API", icon: <SiFastapi /> },
+      { name: "Tensorflow", icon: <SiTensorflow /> },
+    ],
+    visit: "https://cata-ai.vercel.app/",
   },
 ];
